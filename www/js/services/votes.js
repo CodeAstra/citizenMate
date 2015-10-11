7539890f6d65345ce8d4d18abc17b714a9df421f
B V Satyaram <bvsatyaram@gmail.com>
2015-10-11 12:42:43 +0530

    UI Fixes

--- www/js/services/votes.js
+++ www/js/services/votes.js
@@ -8,36 +8,36 @@ angular.module('starter')
       categoryId: 2,
       question: 'Should India spend more on its space program?',
       authorId: 4,
-      totalAgree: 2345,
+      totalAgree: 9345,
       totalDisagree: 2345
     }, {
       id: 2,
       categoryId: 6,
       question: 'Should computer education be brought to computer education?',
       authorId: 2,
-      totalAgree: 2345,
-      totalDisagree: 2345
+      totalAgree: 5648,
+      totalDisagree: 4568
     }, {
       id: 3,
       categoryId: 4,
       question: 'Do you support same-sex marraige?',
       authorId: 1,
-      totalAgree: 2345,
-      totalDisagree: 2345
+      totalAgree: 3654,
+      totalDisagree: 9587
     }, {
       id: 4,
       categoryId: 2,
       question: 'Should all taxes be merged into a single tax?',
       authorId: 4,
-      totalAgree: 2345,
+      totalAgree: 7845,
       totalDisagree: 2345
     }, {
       id: 5,
       categoryId: 7,
       question: 'Do you favour increased development of nuclear power plants in India?',
       authorId: 5,
-      totalAgree: 2345,
-      totalDisagree: 2345
+      totalAgree: 365,
+      totalDisagree: 758
     }];
 
     return {
